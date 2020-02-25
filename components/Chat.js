03ac6c67e8be6dd15969bfd1a7174fa7903b3091
Pierre Segalen <pierre.segalen@gmail.com>
2020-02-25 10:45:07 +0100

    Made the view aware of the keyboard height

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,138 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+  Keyboard
+} from "react-native";
+import * as firebase from "firebase";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { ActivityIndicator } from "react-native-paper";
 
 const Chat = props => {
   const room = props.navigation.getParam("room", {});
+
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [isSending, setIsSending] = useState(false);
+  const [currentRoomId, setCurrentRoomId] = useState("");
+  const [token, setToken] = useState("");
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+  const [keyboardListener1, setKeyboadListener1] = useState(null);
+  const [keyboardListener2, setKeyboadListener2] = useState(null);
+
+  if (firebase.auth().currentUser) {
+    firebase
+      .auth()
+      .currentUser.getIdToken()
+      .then(fbToken => {
+        if (token !== fbToken) {
+          console.log("New user token : ", fbToken);
+          setToken(fbToken);
+        }
+      });
+  }
+
+  const getMessages = roomId => {
+    const db = firebase.firestore();
+    db.collection("rooms")
+      .doc(roomId)
+      .onSnapshot(doc => {
+        const messages = [];
+        doc.data().messages.forEach(message => {
+          messages.push(message);
+        });
+        setMessages(messages);
+      });
+  };
+
+  const sendMessage = () => {
+    if (message.length === 0) Alert.alert("Empty message !");
+    else {
+      setIsSending(true);
+      // use API!
+      fetch("https://europe-west1-ynovb3web.cloudfunctions.net/postMessage", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          FirebaseToken: token
+        },
+        body: JSON.stringify({
+          text: message,
+          roomId: room.id
+        })
+      })
+        .then(response => {
+          response.json().then(obj => console.log(obj));
+          setMessage("");
+          setIsSending(false);
+        })
+        .catch(error => {
+          Alert.alert(error.message);
+          console.error(error);
+          setIsSending(false);
+        });
+    }
+  };
+
+  if (room.id !== currentRoomId) {
+    getMessages(room.id);
+    setCurrentRoomId(room.id);
+  }
+
+  if (keyboardListener1 === null) {
+    setKeyboadListener1(
+      Keyboard.addListener("keyboardDidShow", evt => {
+        setKeyboardHeight(Math.round(evt.endCoordinates.height));
+      })
+    );
+  }
+
+  if (keyboardListener2 === null) {
+    setKeyboadListener2(
+      Keyboard.addListener("keyboardDidHide", evt => {
+        setKeyboardHeight(0);
+      })
+    );
+  }
+
+  console.log(`Keyboard height: ${keyboardHeight}`);
+
   return (
-    <View>
-      <Text style={{ fontSize: 24, textAlign: "center" }}>
-        Room {room.name}
-      </Text>
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={messages}
+        renderItem={({ item }) => (
+          <Text style={{ fontSize: 20, marginBottom: 16 }}>{item.text}</Text>
+        )}
+        keyExtractor={item => item.time.toString()}
+        contentContainerStyle={{ padding: 16 }}
+        style={{ flex: 1 }}
+      />
+      <View
+        style={{
+          padding: 16,
+          paddingBottom: 16 + keyboardHeight,
+          flexDirection: "row"
+        }}
+      >
+        <TextInput
+          style={{ flex: 1, borderWidth: 1, marginRight: 16 }}
+          value={message}
+          onChangeText={text => setMessage(text)}
+        />
+        {isSending ? (
+          <ActivityIndicator color="#000" />
+        ) : (
+          <TouchableOpacity onPress={sendMessage}>
+            <MaterialCommunityIcons name="send" size={30} color="#000" />
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 };
